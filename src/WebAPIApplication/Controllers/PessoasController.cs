a63bd75202b603e1a60e8a06c3f75dceef4e12e9
André stoicov <stoicov.asr@gmail.com>
2016-12-01 23:18:43 -0200

    ajuste variavel yml

--- src/WebAPIApplication/Controllers/PessoasController.cs
+++ src/WebAPIApplication/Controllers/PessoasController.cs
@@ -1,35 +1,77 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Net;
 
-namespace WebAPIApplication{
-    
+namespace WebAPIApplication
+{
     [Route("api/pessoas")]
     public class PessoasController : Controller
     {
+        private readonly DataContext _dataContext;
 
-     private readonly DataContext _dataContext;
+        public PessoasController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
 
-     public PessoasController(DataContext dataContext)
-     {
-         _dataContext = dataContext;
-     }
+        [HttpGet]
+        public async Task<IActionResult> ObterPessoas()
+        {
+            var pessoas = await _dataContext.Pessoas.ToListAsync();
+            return Json(pessoas);
+        }
 
-     [HttpGet]
-     public async Task<IActionResult> GetPessoas()
-     {
-         var pessoas = await _dataContext.Pessoas.ToListAsync();
-         return Json(pessoas);
-     }
+        [HttpPost]
+        public async Task<IActionResult> CriaPessoa([FromBody]Pessoa modelo)
+        {
+            await _dataContext.Pessoas.AddAsync(modelo);
+            await _dataContext.SaveChangesAsync();
 
-     [HttpPost]
-     public async Task<IActionResult> CriaPessoa([FromBody]Pessoa modelo)
-     {
-        await _dataContext.Pessoas.AddAsync(modelo);
-        await _dataContext.SaveChangesAsync();
+            return Json(modelo);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObterPessoa(int id)        
+        {
+            var pessoa = await  _dataContext.Pessoas.SingleOrDefaultAsync(x=> x.Id == id);    
+            
+            if(pessoa == null)
+                return NotFound();
 
-        return Json(modelo);
-     }
+            return Json(pessoa);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizaPessoa(int id, [FromBody]Pessoa modelo)        
+        {
+            var pessoa = await  _dataContext.Pessoas.SingleOrDefaultAsync(x=> x.Id == id);    
+            
+            if(pessoa == null)
+                return NotFound();
 
+            pessoa.Nome = modelo.Nome;
+            pessoa.Twitter = modelo.Twitter;
+
+            await _dataContext.SaveChangesAsync();
+
+            return Json(pessoa);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemovePessoa(int id)        
+        {
+            var pessoa = await _dataContext.Pessoas.SingleOrDefaultAsync(x=> x.Id == id);
+
+            if(pessoa == null)
+                return NotFound();
+
+            _dataContext.Pessoas.Remove(pessoa);
+
+            await _dataContext.SaveChangesAsync();
+
+            return StatusCode((int)HttpStatusCode.OK);
+        }
     }
 }
